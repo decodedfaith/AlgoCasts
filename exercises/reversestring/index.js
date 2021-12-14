17097b5585370d82c7ca82e8152a9e7815edb321
Faith Adetunji Komolafe <komolafefaith@gmail.com>
2021-12-14 18:47:51 +0100

    Update index.js
    reverseString lesson done

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,28 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return str.split('').reduce((rev, char)=> char + rev, '');
+}
 
 module.exports = reverse;
+
+//Solution 1
+// function reverse(str) {
+//     // str = str.trim();
+//     let reversed = '';
+//     for (let i = str.length - 1; i >= 0; i--){
+//         reversed += str[i];
+//     }
+//     return reversed;
+// }
+
+//Solution 2
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+//   }
+
+//Solution 3
+// function reverse(str) {
+//     return str.split('').reduce((rev, char)=> char + rev, '');
+// }
